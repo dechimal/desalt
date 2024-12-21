722e8ab3a389656c0fb3633359b4502013ffa422
d <d.g.feat.th2iinchoiincho@gmail.com>
2024-12-21 17:41:30 +0900

    quectotest: 修正

--- include/desalt/quectotest.hpp
+++ include/desalt/quectotest.hpp
@@ -13,6 +13,18 @@
 #include <ranges>
 #endif
 
+#define TEST(test_case_name, ...) TEST_I(QCT_CAT(qcttest_, __COUNTER__), test_case_name __VA_OPT__(,) __VA_ARGS__)
+#define TEST_I(class_name, test_case_name, ...) \
+    struct class_name __VA_OPT__(:) __VA_ARGS__ { \
+        void _qctst_test_case(); \
+        static inline constexpr char _qctst_test_case_name[] = test_case_name; \
+        __attribute__((constructor)) static void _qctst_init() { \
+            ::desalt::qcttest::_qctst_init<class_name>(); \
+        } \
+    }; \
+    void class_name::_qctst_test_case() \
+    //
+
 #define ASSERT_EQ(x, y, ...) ASSERT_BIN((x), (y), #x, #y, == __VA_OPT__(,) __VA_ARGS__)
 #define ASSERT_NE(x, y, ...) ASSERT_BIN((x), (y), #x, #y, != __VA_OPT__(,) __VA_ARGS__)
 
@@ -20,7 +32,7 @@
     ASSERT_IMPL( \
         (x), \
         #x, \
-        ("  value: {}\n", qcttest::fmt_wrap{x}), \
+        ("  value: {}\n", ::desalt::qcttest::fmt_wrap{x}), \
         __VA_OPT__(std::println(__VA_ARGS__)) \
     ) \
     //
@@ -28,7 +40,7 @@
     ASSERT_IMPL( \
         (x op y), \
         xstr " " #op " " ystr, \
-        ("  {}: {}\n  {}: {}", xstr, qcttest::fmt_wrap{x}, ystr, qcttest::fmt_wrap{y}), \
+        ("  {}: {}\n  {}: {}", xstr, ::desalt::qcttest::fmt_wrap{x}, ystr, ::desalt::qcttest::fmt_wrap{y}), \
         __VA_OPT__(std::println(__VA_ARGS__)) \
     ) \
     //
@@ -40,24 +52,36 @@
 #define QCT_LINE_STR QCT_STR(__LINE__)
 #define ASSERT_IMPL(e, estr, diag_msg, user_msg) \
     do if (!(e)) { \
-        std::println("FAILED: " __FILE__ ":" QCT_LINE_STR ": case \"{}\"\n  expr: {}", qcttest::current_name, estr); \
+        std::println("FAILED: " __FILE__ ":" QCT_LINE_STR ": case \"{}\"\n  expr: {}", ::desalt::qcttest::current_name, estr); \
         std::println diag_msg; \
         user_msg; \
-        qcttest::current_failure = true; \
+        ::desalt::qcttest::current_failure = true; \
         return; \
     } while (0) \
     //
 
-namespace qcttest {
+namespace desalt::qcttest {
+namespace here = qcttest;
 
 struct test_link {
     void (*test)();
-    char const * name = nullptr;
-    test_link * next = nullptr;
+    char const * name;
+    test_link * next;
 };
-static test_link * first, * last;
-static bool current_failure = false;
-static char const * current_name = nullptr;
+static constinit test_link * first, * last;
+static constinit bool current_failure;
+static constinit char const * current_name;
+
+template<typename Derived>
+static void _qctst_init() {
+    static constinit test_link link = test_link {
+        [] { Derived()._qctst_test_case(); },
+        Derived::_qctst_test_case_name,
+        nullptr,
+    };
+    if (!here::first) here::first = here::last = &link;
+    else here::last = here::last->next = &link;
+}
 
 template<typename T>
 struct fmt_wrap {
@@ -79,23 +103,21 @@ template<typename CharT, std::size_t N> struct is_string_family<CharT const [N],
 template<typename T, typename CharT>
 concept string_family = is_string_family<T, CharT>::value;
 
-}
-
-namespace std {
+template<typename T, typename CharT> struct formatter;
 
 template<typename CharT, std::formattable<CharT> T>
-    requires (!std::ranges::input_range<T>) || qcttest::string_family<T, CharT>
-struct formatter<qcttest::fmt_wrap<T>, CharT>: std::formatter<T, CharT> {
+    requires (!std::ranges::input_range<T>) || string_family<T, CharT>
+struct formatter<T, CharT>: std::formatter<T, CharT> {
     template<typename FC>
-    typename FC::iterator format(qcttest::fmt_wrap<T> const & x, FC & fc) const {
+    typename FC::iterator format(fmt_wrap<T> const & x, FC & fc) const {
         return std::formatter<T, CharT>::format(*x.ptr, fc);
     }
 };
 
 template<typename T, typename CharT>
-struct formatter<qcttest::fmt_wrap<T *>, CharT>: std::formatter<void const *, CharT> {
+struct formatter<T *, CharT>: std::formatter<void const *, CharT> {
     template<typename FC>
-    typename FC::iterator format(qcttest::fmt_wrap<T *> const & x, FC & fc) const {
+    typename FC::iterator format(fmt_wrap<T *> const & x, FC & fc) const {
         return std::formatter<void const *, CharT>::format(*x.ptr, fc);
     }
 };
@@ -105,12 +127,12 @@ template<typename T, typename CharT>
         requires (std::basic_ostream<CharT> & ost, T t) { ost << t; }
         && (!std::formattable<T, CharT>)
         && (!std::ranges::input_range<T> || !std::formattable<std::ranges::range_value_t<T>, CharT>)
-struct formatter<qcttest::fmt_wrap<T>, CharT> {
+struct formatter<T, CharT> {
     constexpr auto parse(auto & pc) {
         return pc.begin();
     }
     template<typename FC>
-    typename FC::iterator format(qcttest::fmt_wrap<T> x, FC & fc) const {
+    typename FC::iterator format(fmt_wrap<T> x, FC & fc) const {
         std::basic_stringstream<CharT> ss;
         ss << *x.ptr;
         auto s = std::move(ss.str());
@@ -121,12 +143,13 @@ struct formatter<qcttest::fmt_wrap<T>, CharT> {
 #if __GNUC__ <= 14
 
 template<std::ranges::input_range R, typename CharT>
-    requires std::formattable<std::ranges::range_value_t<R>, CharT> && (!qcttest::string_family<R, CharT>)
-struct formatter<qcttest::fmt_wrap<R>, CharT> {
+    requires std::formattable<std::ranges::range_value_t<R>, CharT>
+        && (!string_family<R, CharT>)
+struct formatter<R, CharT> {
     constexpr auto parse(auto & pc) {
         return pc.begin();
     }
-    auto format(qcttest::fmt_wrap<R> const & r, auto & fc) const {
+    auto format(fmt_wrap<R> const & r, auto & fc) const {
         auto out = fc.out();
         *out++ = '[';
         bool trailing = false;
@@ -142,31 +165,19 @@ struct formatter<qcttest::fmt_wrap<R>, CharT> {
 
 #endif
 
-}
+} // namespace desalt::qcttest
 
-#define TEST(test_case_name, ...) TEST_I(QCT_CAT(qcttest_, __COUNTER__), test_case_name __VA_OPT__(,) __VA_ARGS__)
-#define TEST_I(class_name, test_case_name, ...) \
-    struct class_name __VA_OPT__(:) __VA_ARGS__ { \
-        void _qct_test_case(); \
-        static void _qct_run() { \
-            qcttest::current_failure = false; \
-            qcttest::current_name = test_case_name; \
-            try { \
-                class_name()._qct_test_case(); \
-            } catch (...) { \
-                qcttest::current_failure = true; \
-            } \
-        } \
-        __attribute__((constructor)) static void _qct_init() { \
-            static qcttest::test_link link{_qct_run, test_case_name}; \
-            if (!qcttest::first) qcttest::first = qcttest::last = &link; \
-            else qcttest::last = qcttest::last->next = &link; \
-        } \
-    }; \
-    void class_name::_qct_test_case() \
-    //
+namespace std {
+
+template<typename T, typename CharT>
+struct formatter<desalt::qcttest::fmt_wrap<T>, CharT>
+    : desalt::qcttest::formatter<T, CharT>
+{};
+
+} // namespace std
 
 int main(int argc, char * argv[]) {
+    namespace qct = desalt::qcttest;
     std::vector<std::regex> pats;
     if (argc < 2) {
         pats.emplace_back(".*");
@@ -175,14 +186,21 @@ int main(int argc, char * argv[]) {
     }
 
     bool r = false;
-    for (qcttest::test_link * link = qcttest::first; link; link = link->next) {
+    for (qct::test_link * link = qct::first; link; link = link->next) {
         for (auto & pat: pats) {
-            if (std::regex_match(link->name, pat)) {
-                link->test();
-                break;
-            }
+            if (std::regex_match(link->name, pat)) goto found;
         }
-        r |= qcttest::current_failure;
+        continue;
+    found:
+        qct::current_failure = false;
+        qct::current_name = link->name;
+        try {
+            link->test();
+        } catch (...) {
+            std::println("FAILED: exception throw from test case \"{}\"", qct::current_name);
+            qct::current_failure = true;
+        }
+        r |= qct::current_failure;
     }
     return r;
 }
